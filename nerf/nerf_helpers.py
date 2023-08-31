727182a2cc3132a100c21a1d908f2d411810a9dd
haefner <bjoern.haefner@tum.de>
2023-08-31 16:08:42 +0200

    Accelerate get_ray_bundle()

--- nerf/nerf_helpers.py
+++ nerf/nerf_helpers.py
@@ -103,9 +103,7 @@ def get_ray_bundle(
         ],
         dim=-1,
     )
-    ray_directions = torch.sum(
-        directions[..., None, :] * tform_cam2world[:3, :3], dim=-1
-    )
+    ray_directions = torch.einsum("mn,hwn->hwm", tform_cam2world[:3, :3], directions)
     ray_origins = tform_cam2world[:3, -1].expand(ray_directions.shape)
     return ray_origins, ray_directions
 
