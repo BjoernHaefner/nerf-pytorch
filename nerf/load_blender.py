f9786195fd6a6fc920482a7c324f895fbbd9cd21
haefner <bjoern.haefner@tum.de>
2023-08-31 16:54:56 +0200

    imageio.imread -> imageio.v2.imread

--- nerf/load_blender.py
+++ nerf/load_blender.py
@@ -58,7 +58,7 @@ def load_blender_data(basedir, half_res=False, testskip=1, debug=False):
 
         for frame in meta["frames"][::skip]:
             fname = os.path.join(basedir, frame["file_path"] + ".png")
-            imgs.append(imageio.imread(fname))
+            imgs.append(imageio.v2.imread(fname))
             poses.append(np.array(frame["transform_matrix"]))
         imgs = (np.array(imgs) / 255.0).astype(np.float32)
         poses = np.array(poses).astype(np.float32)
